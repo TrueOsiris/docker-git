c2476cb66f4cdf643ca5ef24d82d8d0bf3cbb0d3
trueosiris <tim@chaubet.be>
2022-03-29 16:04:10 +0000

    changing root homedir

--- start.sh
+++ start.sh
@@ -15,6 +15,8 @@ echo $'/var/log/cron.log {\n  rotate 7\n  daily\n  missingok\n  notifempty\n  cr
 echo "$date Running start.sh" >> /var/log/cron.log
 echo "30 5 * * * /usr/sbin/logrotate /etc/logrotate.d/git-cron" >> /etc/cron.d/git-cron
 echo " " >> /etc/cron.d/git-cron
+/usr/bin/sed '/root/s!\(.*:\).*:\(.*\)!\1/mnt/github:\2!' /etc/passwd > /etc/passwd2
+/usr/bin/mv /etc/passwd2 /etc/passwd
 /usr/bin/crontab /etc/cron.d/git-cron
 /usr/sbin/cron -f
 /usr/bin/tail -f /var/log/cron.log
