1ff4ca7b681c5d6f084eade9039ebcee32e7556d
trueosiris <tim@chaubet.be>
2022-03-30 13:07:36 +0000

    tampering with github

--- start.sh
+++ start.sh
@@ -37,8 +37,9 @@ fi
 /usr/bin/cp /root/.* /mnt/github/ 2>/dev/null
 /usr/bin/ln -s /gitpush /usr/bin/gitpush
 /usr/bin/ln -s /dockerpush /usr/bin/dockerpush
-/usr/bin/mkdir /mnt/github/.ssh
-/usr/bin/ssh-keygen -t ed25519 -C "$gitmail" -P "" -f /mnt/github/.ssh/id_ed25519
+/usr/bin/mkdir /mnt/github/.ssh 2>/dev/null
+/usr/bin/ssh-keygen -t ed25519 -C "$gitmail" -P "" -f /mnt/github/.ssh/id_ed25519 2>/dev/null
+/usr/bin/cat /mnt/github/.ssh/id_ed25519.pub
 echo $'/var/log/cron.log {\n  rotate 7\n  daily\n  missingok\n  notifempty\n  create\n}' > /etc/logrotate.d/git-cron
 echo "$date Running start.sh" >> /var/log/cron.log
 echo "30 5 * * * /usr/sbin/logrotate /etc/logrotate.d/git-cron" >> /etc/cron.d/git-cron
