ee29535a5f6a68704e4dec33e06dd1eb6fd6e48a
trueosiris <tim@chaubet.be>
2022-10-11 10:45:07 +0200

    ssh server

--- start.sh
+++ start.sh
@@ -87,6 +87,7 @@ echo " " >> /etc/cron.d/git-cron
 /usr/bin/sed '/root/s!\(.*:\).*:\(.*\)!\1/mnt/github:\2!' /etc/passwd > /etc/passwd2
 /usr/bin/mv /etc/passwd2 /etc/passwd 2>&1
 /usr/bin/sed -i 's/#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config 2>&1
+/etc/init.d/ssh start 2>&1
 /usr/bin/crontab /etc/cron.d/git-cron
 /usr/sbin/cron -f
 /usr/bin/tail -f /var/log/cron.log
